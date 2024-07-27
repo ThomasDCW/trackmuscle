654f16ba86095a47f33fdb7ebe61f68512db6ef1
Thomas Di Cola <thomas.dicola@user-agency.com>
2024-07-27 15:25:26 +0200

    init

---
+++ components/Typography/H1.tsx
@@ -0,0 +1,11 @@
+interface H1Props {
+  label: string;
+}
+
+export function H1({ label }: H1Props) {
+  return (
+    <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-5xl">
+      {label}
+    </h1>
+  );
+}
